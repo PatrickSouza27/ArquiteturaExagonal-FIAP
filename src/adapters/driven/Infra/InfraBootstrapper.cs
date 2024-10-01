3b54d35aa76a025e0f4b56bfe251b2aeab539e6f
Patrick <patrickaquinodesouza@gmail.com>
2024-10-01 16:22:18 -0300

    arquitetura hexagonal att

---
+++ src/adapters/driven/Infra/InfraBootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using Infra.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra;
+
+public static class InfraBootstrapper
+{
+    public static void Register(IServiceCollection services)
+    {
+        services.AddTransient<IAuthenticatorRepository, AuthenticatorRepository>();
+    }
+}
